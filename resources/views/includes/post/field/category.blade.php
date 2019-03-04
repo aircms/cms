f402d1db7ac1f41fbec921a18818fbcec09da3b4
Rogee <rogeecn@gmail.com>
2019-03-04 22:56:16 +0800

    add category

---
+++ resources/views/includes/post/field/category.blade.php
@@ -0,0 +1,20 @@
+@include('includes.post.field.common.header')
+
+@php
+    $inputName = array_get($input, 'name', "");
+    $inputValue = array_get($input, 'value', []);
+    $inputAttributes = array_get($input, 'attributes', []);
+
+    $labelName = array_get($label, 'name', "");
+    $labelAttributes = array_get($label, 'attributes', []);
+
+    $value = isset($post) ? $post->getMeta($inputName,$inputValue) : $inputValue;
+
+    $items = array_get($input,'items');
+@endphp
+
+{{ html()->multiselect($inputName,$items,$value)->attributes($inputAttributes) }}
+
+
+@include('includes.post.field.common.footer')
+
