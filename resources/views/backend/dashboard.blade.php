b67cf17076889974f015f64d01cc376e68d71bba
Rogee <rogeecn@gmail.com>
2019-04-08 22:49:54 +0800

    remove useless files

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -3,21 +3,6 @@
 @section('title', app_name() . ' | ' . '管理仪表板')
 
 @section('content')
-    <div class="row image-list">
-
-        <div class="col-3 image-uploader">
-            <div class="card">
-                <input type="file" id="file" class="d-none" multiple>
-                <label class="m-0 bg-light img-container" for="file" style="cursor: pointer;">
-                    <div class="text-muted text-center align-middle py-3">
-                        <i class="fa fa-plus fa-3x"></i>
-                    </div>
-                </label>
-            </div>
-        </div>
-    </div>
-
-
     <div class="row mt-5">
         <div class="col">
             <div class="card">
